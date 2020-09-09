4a7bc33ea0cf494dc888db606fb1094f833bddd8
Leigh Larson <leighlarson4@gmail.com>
2020-09-08 21:56:05 -0600

    Add passing test to Search

--- src/Search/Search.test.tsx
+++ src/Search/Search.test.tsx
@@ -1,3 +1,16 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, getByPlaceholderText } from "@testing-library/react";
 import Search from './Search'
+
+describe('Search', () => {
+  beforeEach(() => {
+    render(<Search />)
+  })
+
+  it('should render a search bar', () => {
+    const search = screen.getByPlaceholderText('Search city, zip, or county')
+    expect(search).toBeInTheDocument()
+  })
+
+  // it('should ')
+})

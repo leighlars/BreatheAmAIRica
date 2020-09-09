3792ad46ca4edf8812833fe65efcbe5e8a823f19
Leigh Larson <leighlarson4@gmail.com>
2020-09-08 22:03:43 -0600

    Add passing test to Header for logo and search bar

--- src/Header/Header.test.tsx
+++ src/Header/Header.test.tsx
@@ -7,8 +7,16 @@ describe('Header', () => {
     render(<Header/>)
   })
 
-  it('should render app name in Header', () => {
+  it('should render logo and search bar in Header', () => {
     const header = screen.getByRole('heading', {name: 'Weather Or Not'})
+    const globe = screen.getByRole('img')
+    const tagline = screen.getByRole('heading', {name: 'Plan. Research. Experience.'})
+    const searchBar = screen.getByPlaceholderText('Search city, zip, or county')
     expect(header).toBeInTheDocument()
+    expect(globe).toBeInTheDocument()
+    expect(tagline).toBeInTheDocument()
+    expect(tagline).toBeInTheDocument()
   })
+
+
 })

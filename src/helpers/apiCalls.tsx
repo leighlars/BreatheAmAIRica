d0c8182ad2198fc84b7c246e3abb90b7e8ebb066
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-08 20:57:48 -0600

    Update getCoordinates to use async await #7

--- src/helpers/apiCalls.tsx
+++ src/helpers/apiCalls.tsx
@@ -1,10 +1,11 @@
-export const getCoordinates = () => {
-	return fetch(`http://api.positionstack.com/v1/forward?access_key=e17943cbd88c595c58c3c6ae1840fc33&query=${query}`)
-		.then(response => {
+export const getCoordinates = async (query: string) => {
+	const response = await fetch(`http://api.positionstack.com/v1/forward?access_key=e17943cbd88c595c58c3c6ae1840fc33&query=${query}`)
+
 			if (response.ok) {
-				return response.json()
+				const data = await response.json()
+				const coordinates = [data.data[0].latitude, data.data[0].longitude]
+				return coordinates
 			} else {
-				throw response
+				throw new Error(response.statusText)
 			}
-		})
 }

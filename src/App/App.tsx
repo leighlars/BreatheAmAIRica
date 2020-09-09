b2f755d030b68ce963658f61c499b79b08054ad1
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-08 22:03:36 -0600

    Add method to get top 5 cities #9

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,13 +3,26 @@ import { Route } from 'react-router-dom'
 import './App.scss'
 import Header from '../Header/Header'
 import View from '../View/View'
+import { getCoordinates } from '../helpers/apiCalls'
 
 const App: React.FC = () => {
+	// const [ topCities, setTopCities ] = useState([])
+
+	useEffect(() => {getTop5Cities()}, [])
+
+	const getTop5Cities = async ():Promise<any> => {
+		const topCities = ['Denver']
+		const topCitiesData = topCities.map(city => {
+			return getCoordinates(city)
+		})
+		return topCitiesData
+	}
+
   return (
     <div className="App">
       <Header/>
 			<Route
-				exact path='/'
+				exact path="/"
 				render={() => {
 					return <View />
 				}}

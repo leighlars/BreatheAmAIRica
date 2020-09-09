2ab71638d3ceb14a4cab0ced48cabb02491fd37c
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-08 21:15:45 -0600

    Update App component to account for TS

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom'
-import './App.scss';
+import './App.scss'
 import Header from '../Header/Header'
 import View from '../View/View'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Header/>
-			<Route exact path='/' render={() => {
-				return <View />
-			}}/>
+			<Route
+				exact path='/'
+				render={() => {
+					return <View />
+				}}
+			/>
     </div>
-  );
+  )
 }
 
 export default App

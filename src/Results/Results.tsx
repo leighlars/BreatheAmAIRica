ebba97379385c21ca719c83a2f9e17bc72795e7a
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-10 16:10:04 -0600

    Add component structure for Results #20

---
+++ src/Results/Results.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { getCoordinates } from '../helpers/apiCalls'
+import './Results.scss'
+
+const Results: React.FC = () => {
+
+	return (
+		<section className="results">
+			Results Go Here
+		</section>
+	)
+}
+
+export default Results

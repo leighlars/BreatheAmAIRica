314834e18cf9f51b92617d40409a3cd98637934c
Leigh Larson <leighlarson4@gmail.com>
2020-09-10 15:31:38 -0600

    Hardcode coordinates for cities

--- src/helpers/cities.ts
+++ src/helpers/cities.ts
@@ -1,83 +1,83 @@
 export const popularCities = [ 
   {
     name: "Denver" ,
-    lat: 39.77,
-    long: -105.00,
+    lat: 39.7794,
+    long: -105.00523,
   },
   {
     name: "San Francisco",
-    lat: 37.76,
-    long: -122.39,
+    lat: 37.7658,
+    long: -122.3978,
   },
   {
     name: "Miami",
-    lat: 25.79,
-    long: -80.21,
+    lat: 25.7947,
+    long: -80.2156,
   },
   {
     name: "Houston",
-    lat: 29.90,
-    long: -95.32,
+    lat: 29.9011,
+    long: -95.3261,
   },
   {
     name: "New York City",
-    lat: 40.69,
-    long: -73.92,
+    lat: 40.694401,
+    long: -73.928596,
   }
 ]
 
 export const lowOzoneCities = [
   {
     name: "Anchorage, AK", 
-    lat: 61.21, 
-    long: -149.90
+    lat: 61.2181, 
+    long: -149.9003
   }, 
   {
     name: "Savannah, GA",
-    lat: '', 
-    long: ''
+    lat: 32.0809, 
+    long: -81.0912
   }, 
   {
     name: "Destin, FL",
-    lat: '', 
-    long: ''
+    lat: 30.3935, 
+    long: -86.4958
   }, 
   {
     name: "Honolulu, HI",
-    lat: '', 
-    long: '' 
+    lat: 21.3069, 
+    long: -157.8583
   }, 
   {
     name: "Casper, WY",
-    lat: '', 
-    long: ''
+    lat: 42.8501, 
+    long: -106.3252
   }
   ]
 
 export const lowPollutionCities = [
   {
     name: "Hot Springs, AR",
-    lat: '', 
-    long: ''
+    lat: 34.5037,
+    long: -93.0552
   }, 
   {
     name: "Colorado Springs, CO",
-    lat: '', 
-    long: ''
+    lat: 38.8339, 
+    long: -104.8214
   }, 
   {
     name: "Bellingham, WA",
-    lat: '', 
-    long: ''
+    lat: 48.7519, 
+    long: -122.4787
   },
   {   
     name: "Albuquerque, NM",
-    lat: '', 
-    long: ''
+    lat: 35.0844, 
+    long: -106.6504
   },
   {
     name: "Portland, ME",
-    lat: '', 
-    long: ''
+    lat: 43.6591, 
+    long: - 70.2568
   }
 ]

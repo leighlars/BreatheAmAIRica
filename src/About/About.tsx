2e86a1b4f31c823aac14fcea014d9cb421ef39a4
Leigh Larson <leighlarson4@gmail.com>
2020-09-09 19:30:50 -0600

    Add credits section to bottom of About

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -4,16 +4,34 @@ import aqtable from '../assets/aqtable2.jpeg'
 
 const About: React.FC = () => {
   return (
-    <section className='about-section'>
-      <span className='intro-text'>
-        <h2>Plan. Research. Experience.</h2>
-        <p>At <b>Weather or Not</b>, we know how important it is to you 
-        and your loved ones to have accurate weather and air quality information. <br/> <br/>
-        Our data is updated every 10-60 minutes, giving you the most updated conditions to help you plan for your enjoyment and peace of mind.
-        </p>
-      </span>
-      <img src={aqtable} className='aq-image' alt='table of Air Quality information from EPA.gov'/>
-    </section>)
+   <section className="about-section">
+    <span className="intro-text">
+     <h2>Plan. Research. Experience.</h2>
+     <p>
+      At <b>Weather or Not</b>, we know how important it is to you and your
+      loved ones to have accurate weather and air quality information. <br />{" "}
+      <br />
+      Our data is updated every 10-60 minutes, giving you the most updated
+      conditions to help you plan for your enjoyment and peace of mind.
+     </p>
+    </span>
+    <img
+     src={aqtable}
+     className="aq-image"
+     alt="table of Air Quality information from EPA.gov"
+    />
+    <span className="credits">
+     <p>App Developed by:</p>
+     <br />
+     <a href="https://github.com/ErinUntermeyer">Erin Untermeyer</a>
+     <a href="https://github.com/JoshSevy">Josh Sevy</a>
+     <a href="https://github.com/leighlars">Leigh Larson</a> <br /> <br />
+     <p>Data Sources:</p>
+     <a href="https://www.iqair.com/us/">IQAir</a>{" "}
+     <a href="https://openweathermap.org/">Open Weather Map</a>
+    </span>
+   </section>
+  );
 }
 
 export default About

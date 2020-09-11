80efcb508b80a18f237d43cc0f16e7809199fe88
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-10 20:36:37 -0600

    Add shell for testing suite #20

---
+++ src/Results/Results.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Results from './Results';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Results', () => {
+	let searchResults
+
+	beforeEach(() => {
+		searchResults = [
+			{
+				'latitude': 40,
+				'longitude': -110,
+				'name': 'Denver',
+				'country': 'United States',
+				'continent': 'North America',
+				'label': 'Denver, CO, USA'
+			},
+			{
+				'latitude': 37,
+				'longitude': -140,
+				'name': 'Miami',
+				'country': 'United States',
+				'continent': 'North America',
+				'label': 'Miami, FL, USA'
+			}
+		]
+	})
+
+	it('Should render results from search', () => {
+		const { getByText } = render(
+			<MemoryRouter>
+				<Results
+					searchResults={[]}
+				/>
+			</MemoryRouter>
+		)
+	})
+
+})

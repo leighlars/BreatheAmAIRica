23dda51e62c37ea9748eb88c82330d4c25f89dcb
Leigh Larson <leighlarson4@gmail.com>
2020-09-11 19:08:33 -0600

    Add color to text if uvi or aqi is certain index score

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -18,38 +18,70 @@ const Card: React.FC<CardProps> = (props) => {
   const uvIndex = (uvi: number) => {
     if (uvi <= 2) {
      return (
-      <h3 className="card-uvi-low">
+      <h3 className="card-low">
        <b>{uvi}</b>{" "}
       </h3>
      );
     } else if (uvi >= 3 && uvi <= 5) {
      return (
-      <h3 className="card-uvi-moderate">
+      <h3 className="card-moderate">
        <b>{uvi}</b>
       </h3>
      );
     } else if (uvi === 6 || uvi === 7) {
      return (
-      <h3 className="card-uvi-high">
+      <h3 className="card-high">
        <b>{uvi}</b>
       </h3>
      );
     } else if (uvi >= 8 && uvi <= 10) {
      return (
-      <h3 className="card-uvi-very-high">
+      <h3 className="card-very-high">
        <b>{uvi}</b>
       </h3>
      );
     } else if (uvi >= 11) {
      return (
-      <h3 className="card-uvi-extreme">
+      <h3 className="card-extreme">
        <b>{uvi}</b>
       </h3>
      );
     } 
-
   }
 
+  const aqIndex = (aqi: number) => {
+    if (aqi <= 50) {
+     return (
+      <h3 className="card-low">
+       <b>{aqi}</b>
+      </h3>
+     );
+    } else if (aqi >= 51 && aqi <= 100) {
+     return (
+      <h3 className="card-moderate">
+       <b>{aqi}</b>
+      </h3>
+     );
+    } else if (aqi >= 151 && aqi <= 200) {
+     return (
+      <h3 className="card-high">
+       <b>{aqi}</b>
+      </h3>
+     );
+    } else if (aqi >= 201 && aqi <= 300) {
+     return (
+      <h3 className="card-very-high">
+       <b>{aqi}</b>
+      </h3>
+     );
+    } else if (aqi >= 301) {
+     return (
+      <h3 className="card-extreme">
+       <b>{aqi}</b>
+      </h3>
+     );
+    } 
+  }
 
 
   return (
@@ -62,10 +94,10 @@ const Card: React.FC<CardProps> = (props) => {
      <h2 className="card-header">{props.city.name}</h2>
      <div className='card-air-temp'>
       <h3 className="card-temp">{props.city.temp}&deg;F</h3>
-      <h3 className="card-air"><b>{props.city.aqi}</b> </h3> 
-      <p>AQI</p>
-      {uvIndex(4)}
-      <p>UVI</p>
+      {aqIndex(200)}
+      <p className='unit'>AQI</p>
+      {uvIndex(2)}
+      <p className='unit'>UVI</p>
      </div>
      <img src={clearDay} alt='Sun shining' />
     </article>

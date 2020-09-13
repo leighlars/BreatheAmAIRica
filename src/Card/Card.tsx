c0c0713631352af9e6c902e585c7b62a118d3f3b
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-13 12:10:24 -0600

    Update icon alt text, account for all icons #48

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './Card.scss'
 import clearDay from '../assets/01d.png'
-import clearNight from "../assets/01n.png";
-import cloudyDay from "../assets/02d.png";
-import cloudyNight from "../assets/02n.png";
-import lightClouds from "../assets/03d.png";
-import rain from "../assets/09d.png";
-import sunnyStorms from "../assets/10n.png";
-import thunderstorm from "../assets/11d.png";
-import snow from "../assets/13d.png";
+import clearNight from '../assets/01n.png'
+import cloudyDay from '../assets/02d.png'
+import cloudyNight from '../assets/02n.png'
+import lightClouds from '../assets/03d.png'
+import doubleCloud from '../assets/04d.png'
+import rain from '../assets/09d.png'
+import daySunnyStorm from '../assets/10d.png'
+import sunnyStorms from '../assets/10n.png'
+import thunderstorm from '../assets/11d.png'
+import snow from '../assets/13d.png'
+import mist from '../assets/50d.png'
 // data icon names !== file names
 
 export interface CardProps {
@@ -28,37 +31,37 @@ const Card: React.FC<CardProps> = (props) => {
       <h3 className="card-low">
        <b>{uvi}</b>{" "}
       </h3>
-     );
+     )
     } else if (uvi >= 3 && uvi <= 5) {
      return (
       <h3 className="card-moderate">
        <b>{uvi}</b>
       </h3>
-     );
+     )
     } else if (uvi === 6 || uvi === 7) {
      return (
       <h3 className="card-high">
        <b>{uvi}</b>
       </h3>
-     );
+     )
     } else if (uvi >= 8 && uvi <= 10) {
      return (
       <h3 className="card-very-high">
        <b>{uvi}</b>
       </h3>
-     );
+     )
     } else if (uvi >= 11) {
      return (
       <h3 className="card-extreme">
        <b>{uvi}</b>
       </h3>
-     );
+     )
     } else {
       return (
        <h3 className="card-extreme">
         <b>N/A</b>
        </h3>
-      );
+      )
     }
   }
 
@@ -68,36 +71,36 @@ const Card: React.FC<CardProps> = (props) => {
       <h3 className="card-low">
        <b>{aqi}</b>
       </h3>
-     );
+     )
     } else if (aqi >= 51 && aqi <= 100) {
      return (
       <h3 className="card-moderate">
        <b>{aqi}</b>
       </h3>
-     );
+     )
     } else if (aqi >= 151 && aqi <= 200) {
      return (
       <h3 className="card-high">
        <b>{aqi}</b>
       </h3>
-     );
+     )
     } else if (aqi >= 201 && aqi <= 300) {
      return (
       <h3 className="card-very-high">
        <b>{aqi}</b>
       </h3>
-     );
+     )
     } else if (aqi >= 301) {
      return (
       <h3 className="card-extreme">
        <b>{aqi}</b>
       </h3>
-     );
+     )
     } 
     else {
       return (<h3 className="card-extreme">
        <b>N/A</b>
-      </h3>);
+      </h3>)
     }
   }
 
@@ -105,41 +108,47 @@ const Card: React.FC<CardProps> = (props) => {
    if (temp <= 32) {
     return (
      <h3 className="card-extreme"><b>{temp}</b></h3>
-    );
+    )
    } else if (temp >= 33 && temp <= 59) {
     return (
      <h3 className="card-moderate"><b>{temp}</b></h3>
-    );
+    )
    } else if (temp >= 60 && temp <= 80) {
     return (
      <h3 className="card-high"><b>{temp}</b></h3>
-    );
+    )
    } else if (temp >= 80) {
     return (
      <h3 className="card-very-high"><b>{temp}</b></h3>
-    );
+    )
    }
-  };
+  }
 
   const weatherIcon = (icon: string) => {
-    if (icon === '01d') {
-      return (<img src={clearDay} alt="Sun shining" />);
+    if (icon === "01d") {
+      return (<img src={clearDay} alt="Clear Day Icon" />)
     } else if (icon === "01n") {
-     return <img src={clearNight} alt="Sun shining" />;
+     return <img src={clearNight} alt="Clear Night Icon" />
     } else if (icon === "02d") {
-     return <img src={cloudyDay} alt="Sun shining" />;
+     return <img src={cloudyDay} alt="Cloudy Day Icon" />
     } else if (icon === "02n") {
-     return <img src={cloudyNight} alt="Sun shining" />;
+     return <img src={cloudyNight} alt="Cloudy Night Icon" />
     } else if (icon === "03d") {
-     return <img src={lightClouds} alt="Sun shining" />;
+     return <img src={lightClouds} alt="Light Clouds Icon" />
+    } else if (icon === "04d") {
+     return <img src={doubleCloud} alt="Double Clouds Icon" />
     } else if (icon === "09d") {
-     return <img src={rain} alt="Sun shining" />;
+     return <img src={rain} alt="Rain Icon" />
     } else if (icon === "10d") {
-     return <img src={sunnyStorms} alt="Sun shining" />;
+     return <img src={daySunnyStorm} alt="Day Storm Icon" />
+    } else if (icon === "10n") {
+     return <img src={sunnyStorms} alt="Night Storm Icon" />
     } else if (icon === "11d") {
-     return <img src={thunderstorm} alt="Sun shining" />;
+     return <img src={thunderstorm} alt="Thunderstorm Icon" />
     } else if (icon === "13n") {
-     return <img src={snow} alt="Sun shining" />;
+     return <img src={snow} alt="Snow Icon" />
+    } else if (icon === "50n" || icon === "50d") {
+     return <img src={mist} alt="Mist Icon" />
     }
   }
 
@@ -154,7 +163,7 @@ const Card: React.FC<CardProps> = (props) => {
      <h2 className="card-header">{props.name}</h2>
      <div className="card-air-temp">
       {temp(props.temp)}
-      <p className="unit">&deg;F</p>
+      <p className="unit">&degF</p>
       {aqIndex(props.aqi)}
       <p className="unit">AQI</p>
       {uvIndex(props.uvi)}
@@ -163,7 +172,7 @@ const Card: React.FC<CardProps> = (props) => {
      {weatherIcon(props.icon)}
     </article>
    </Link>
-  );
+  )
 }
 
 export default Card

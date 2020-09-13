6d4c377590f61870f76dbf37735553c80266fab5
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-13 09:18:54 -0600

    Add props to Home #48

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
         <Route
           exact path="/"
           render={() => {
-            return <Home />
+            return <Home query={query}/>
           }}
         />
         <Route

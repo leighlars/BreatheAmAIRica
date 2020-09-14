c0c8b8317b35670e845b7a549a04ff9ce18e563b
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-14 15:47:42 -0600

    Add back in airQuality data #65

--- src/helpers/dataFilter.tsx
+++ src/helpers/dataFilter.tsx
@@ -19,10 +19,16 @@ export const getAllData = async (lat: number, long: number) => {
 export const getHomeData = async (lat: number, long: number) => {
 	const data: any = {}
 	const weather = await getWeatherData(lat, long)
+	const aq = await getAirQualityData(lat, long)
 	data.temp = +((weather.current.temp - 273.15) * 1.8 + 32).toFixed(0)
 	data.uvi = +(Math.round(weather.current.uvi)).toFixed(0)
 	data.icon = weather.current.weather[0].icon
+	if (aq.length !== 0) {
+		data.aqi = aq[0].AQI
+		data.aqiCat = aq[0].Category.Name
+	console.log(data)
 	return data
+	}
 }
 
 

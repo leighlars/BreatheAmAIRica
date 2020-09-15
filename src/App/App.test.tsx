8e5e44989da9ea44ca8256719e6e98d41f15f2f1
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-14 19:02:38 -0600

    Add test that user can click card and go to location page #70

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { act } from 'react-dom/test-utils'
 import App from './App'
-import Home from '../Home/Home'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import { getHomeData } from '../helpers/dataFilter'
+import { getHomeData, getAllData } from '../helpers/dataFilter'
 import { mocked } from 'ts-jest/utils'
 jest.mock('../helpers/dataFilter')
 
 describe('App', () => {
-	let mockHomePageData, data
+	let mockHomePageData, data, mockLocationPageData
 
 	beforeEach(() => {
 		mocked(getHomeData).mockImplementation(() =>
@@ -34,10 +32,55 @@ describe('App', () => {
 			-105,
 			data
 		]
+		mockLocationPageData = {
+			currentWeather: {
+				clouds: 1,
+				dew_point: 278.79,
+				dt: 1600127853,
+				feels_like: 288,
+				humidity: 37,
+				pressure: 1021,
+				sunrise: 1600079776,
+				sunset: 1600124809,
+				temp: 294,
+				uvi: 6.41,
+				visibility: 10000,
+				weather: [{
+					description: 'clear sky',
+					icon: '01n',
+					id: 1,
+					main: 'clear'
+				}],
+				wind_deg: 340,
+				wind_speed: 7.15
+			},
+			weeklyWeather: [{
+				clouds: 86,
+				dew_point: 284.54,
+				dt: 1600099200,
+				feels_like: { day: 293.18, night: 286.94, eve: 289.05, morn: 293.6 },
+				humidity: 46,
+				pop: 0,
+				pressure: 1018,
+				sunrise: 1600079776,
+				sunset: 1600124809,
+				temp: { day: 296.55, min: 292.73, max: 298.58, night: 292.73, eve: 294, morn: 290.14 },
+				uvi: 6.41,
+				weather: [{
+					description: 'overcast clouds',
+					icon: '04d',
+					id: 804,
+					main: 'Clouds'
+				}],
+				wind_deg: 340,
+				wind_speed: 5.33
+			}]
+		}
 	})
 
 	it('Should render a Header', () => {
 		const { getByRole, getByPlaceholderText } = render(<MemoryRouter><App /></MemoryRouter>)
+
 		const logo = getByRole('heading', { name: 'Breathe Am' })
 		const searchBar = getByPlaceholderText('Search city, zip, or county')
 		expect(logo).toBeInTheDocument()
@@ -46,17 +89,15 @@ describe('App', () => {
 
 	it('Should show various pages when nav links are clicked', async () => {
 		const { getByRole, findByText } = render(<MemoryRouter><App /></MemoryRouter>)
-		// Home view with cards
+
 		const denverTopCard = await findByText(/denver/i)
 		expect(denverTopCard).toBeInTheDocument()
 
-		// click About link and render About view
 		const aboutLink = getByRole('link', { name: 'ABOUT' })
 		fireEvent.click(aboutLink)
 		const aqiImage = screen.getByAltText('table of Air Quality information from EPA.gov')
 		expect(aqiImage).toBeInTheDocument()
 
-		// click Home link and return to home
 		const homeLink = screen.getByRole('link', { name: 'HOME' })
 		fireEvent.click(homeLink)
 		expect(getByRole('heading', { name: 'Denver' })).toBeInTheDocument()
@@ -74,6 +115,30 @@ describe('App', () => {
 		expect(title11).toBeInTheDocument()
 		expect(uvi[0]).toBeInTheDocument()
 		expect(uvi[14]).toBeInTheDocument()
+	})
+
+	it('Should display details page when card is clicked', async () => {
+		mocked(getAllData).mockImplementation(() =>
+			Promise.resolve(mockLocationPageData)
+		)
+
+		const { findByText, findByRole } = render(<MemoryRouter><App /></MemoryRouter>)
+
+		const title1 = await findByText(/denver/i)
+		fireEvent.click(title1)
+
+		const happeningNowTitle = await findByRole('heading', { name: /happening now/i })
+		const airQualityTitle = await findByRole('heading', { name: /happening now/i })
+		const weeklyForecastTitle = await findByRole('heading', { name: /happening now/i })
+		expect(happeningNowTitle).toBeInTheDocument()
+		expect(airQualityTitle).toBeInTheDocument()
+		expect(weeklyForecastTitle).toBeInTheDocument()
+
+		const temp = await findByRole('heading', { name: /69/i })
+		expect(temp).toBeInTheDocument()
+		
+		const visibility = await findByText(/1.9 mi/i)
+		expect(visibility).toBeInTheDocument()
 	})
 
 })

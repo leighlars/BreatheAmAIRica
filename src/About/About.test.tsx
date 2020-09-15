2ed3f3d48d80cb8b960b910383664e581ad9210f
Leigh Larson <leighlarson4@gmail.com>
2020-09-14 19:16:16 -0600

    Complete test for About

--- src/About/About.test.tsx
+++ src/About/About.test.tsx
@@ -16,8 +16,33 @@ describe('App', () => {
 	})
 
 	it('should display a summary of site', () => {
-		const summary = screen.getByText(/whether you\'re planning a trip or/i)
+		const summary = screen.getByText(/to plan your local or destination activity/i)
 		expect(summary).toBeInTheDocument()
+	})
+
+	it("should display a aqi legend image", () => {
+  	const legendImage = screen.getByAltText('table of Air Quality information from EPA.gov');
+  	expect(legendImage).toBeInTheDocument();
+	})
+
+	it('should display an icon legend', () => {
+		const iconHeader = screen.getByText('Icon Legend')
+		expect(iconHeader).toBeInTheDocument()
+	})
+
+	it('should display credits box with links to sources and devs', () => {
+		const erin = screen.getByRole('link', {name: 'Erin Untermeyer'})
+		const josh = screen.getByRole("link", { name: "Josh Sevy" })
+		const leigh = screen.getByRole("link", { name: "Leigh Larson" })
+		const aqi = screen.getByRole("link", { name: "AirNow API" })
+		const weather = screen.getByRole("link", { name: "Open Weather Map API" })
+
+		expect(erin).toBeInTheDocument()
+		expect(josh).toBeInTheDocument();
+		expect(leigh).toBeInTheDocument();
+		expect(aqi).toBeInTheDocument();
+		expect(weather).toBeInTheDocument();
+
 	})
 	
 })

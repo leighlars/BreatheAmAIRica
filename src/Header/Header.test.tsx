a7cfc17923fc69c50a36e7f9fad3d3830aab898b
Leigh Larson <leighlarson4@gmail.com>
2020-09-15 14:22:45 -0600

    Add passing tests to Header

--- src/Header/Header.test.tsx
+++ src/Header/Header.test.tsx
@@ -1,22 +1,50 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, getByAltText } from "@testing-library/react";
 import Header from "./Header";
+import '@testing-library/jest-dom'
+import { MemoryRouter } from "react-router-dom";
 
 describe('Header', () => {
-  beforeEach(() => {
-    render(<Header/>)
-  })
+  
+  // beforeEach(() => {
+  //   render(
+  //   <MemoryRouter>
+  //     <Header 
+  //       getSearchResults={jest.fn()}/>
+  //   </MemoryRouter>)
+  // })
 
   it('should render logo and search bar in Header', () => {
-    const logo = screen.getByText('heading', {name: 'Breathe'})
+    render(
+     <MemoryRouter>
+      <Header getSearchResults={jest.fn()} />
+     </MemoryRouter>
+    );
+    const logo = screen.getByRole('heading', {name:'Breathe Am'})
     const globe = screen.getByRole('img')
-    const tagline = screen.getByRole('heading', {name: 'Plan. Research. Experience.'})
+    const tagline = screen.getByRole('heading', {name: 'Plan. Go. Breathe.'})
     const searchBar = screen.getByPlaceholderText('Search city, zip, or county')
+    const aboutLink = screen.getByRole('link', {name:'ABOUT'} )
     expect(logo).toBeInTheDocument()
     expect(globe).toBeInTheDocument()
     expect(tagline).toBeInTheDocument()
     expect(searchBar).toBeInTheDocument()
+    expect(aboutLink).toBeInTheDocument()
   })
+
+  it('should fire an event when search button is clicked', () => {
+    const getSearchResults = jest.fn()
+    render(
+     <MemoryRouter>
+      <Header getSearchResults={getSearchResults} />
+     </MemoryRouter>
+    );
+    const magGlass = screen.getByAltText("magnifying glass search icon");
+    fireEvent.click(magGlass)
+    expect(getSearchResults).toBeCalledTimes(1)
+  })
+
+  
 
 
 })

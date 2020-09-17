55d704995944963f0bbe4041d20e1d0883a414e3
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-17 10:04:11 -0600

    Hide API keys

--- src/helpers/apiCalls.tsx
+++ src/helpers/apiCalls.tsx
@@ -1,10 +1,7 @@
-const airQualityKey = 'DACE2187-D810-4B4B-81E4-45AEBAF087A0'
-const weatherDataKey = '02a1de26e83e798e7595a522e8e4e9d9'
-const weatherDataKey2 = '7b9afd68afe01b32e10130a572c8c564'
-const weatherDataKey3 = '346025de560bd5d671b73a88bec2fce6'
+require('dotenv').config()
 
 export const getCoordinates = (query: string) => {
-	return fetch(`http://api.positionstack.com/v1/forward?access_key=e17943cbd88c595c58c3c6ae1840fc33&query=${query}`)
+	return fetch(`http://api.positionstack.com/v1/forward?access_key=${process.env.REACT_APP_COORDINATES}&query=${query}`)
 	.then(response => {
 		if (response.ok) {
 			return response.json()
@@ -19,7 +16,7 @@ export const getCoordinates = (query: string) => {
 
 export const getWeatherData = async (lat: number, long: number) => {
   return await fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${weatherDataKey3}`
+		`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_KEY3}`
     ).then(parseData => parseData.json()).catch(error => error)
 }
 
@@ -27,7 +24,7 @@ export const getAirQualityData = (lat: number, long: number) => {
    return fetch('https://fe-cors-proxy.herokuapp.com', {
     headers: {
       'Target-URL':
-        `http://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=${lat}&longitude=${long}&API_KEY=${airQualityKey}`,
+				`http://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=${lat}&longitude=${long}&API_KEY=${process.env.REACT_APP_AIR}`,
     },
   })
     .then((response) => response.json())
